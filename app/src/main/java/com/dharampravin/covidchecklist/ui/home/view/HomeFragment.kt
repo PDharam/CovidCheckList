9738c68daf93d41794862294be9d91ed44e35e5c
Pravin Dharam <dharam.pravin@mahindra.com>
2020-03-25 02:49:23 +0530

    #Set multi lingual functionality on dashboard

--- app/src/main/java/com/dharampravin/covidchecklist/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/dharampravin/covidchecklist/ui/home/view/HomeFragment.kt
@@ -1,20 +1,26 @@
 package com.dharampravin.covidchecklist.ui.home.view
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import com.dharampravin.covidchecklist.R
 import com.dharampravin.covidchecklist.corona_checklist.view.CoronaCheckListActivity
+import com.dharampravin.covidchecklist.dashboard.view.DashboardActivity
 import com.dharampravin.covidchecklist.ui.home.presenter.HomeFragmentPresenter
 import com.dharampravin.covidchecklist.ui.home.presenter.HomeFragmentPresenterImpl
 import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
 
 class HomeFragment : Fragment(), HomeFragmentView, View.OnClickListener {
     private lateinit var homeFragmentPresenter: HomeFragmentPresenter
-
+    private var isInitialSpinner = true;
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -38,6 +44,24 @@ class HomeFragment : Fragment(), HomeFragmentView, View.OnClickListener {
 
     override fun setListener() {
         btn_calculate.setOnClickListener(this)
+        spn_language.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (!isInitialSpinner)
+                    setLanguage(position)
+                else
+                    isInitialSpinner = false
+
+            }
+        }
     }
 
     override fun onClick(v: View?) {
@@ -50,4 +74,25 @@ class HomeFragment : Fragment(), HomeFragmentView, View.OnClickListener {
         val i = Intent(activity, CoronaCheckListActivity::class.java)
         startActivity(i)
     }
+
+    private fun setLanguage(position: Int) {
+        var lang = "en"
+        lang = when (position) {
+            0 -> "en"
+            1 -> "hi"
+            2 -> "mr"
+            else -> "en"
+        }
+
+        val myLocale = Locale(lang)
+        val res: Resources = resources
+        val dm: DisplayMetrics = res.getDisplayMetrics()
+        val conf: Configuration = res.getConfiguration()
+        conf.locale = myLocale
+        res.updateConfiguration(conf, dm)
+        val refresh = Intent(activity, DashboardActivity::class.java)
+        activity?.finish()
+        activity?.startActivity(refresh)
+    }
+
 }
