2fcc0bca4694999ac460e0b1f2293a87ae069791
sagar watamkar <akash.triphobo@gmail.com>
2020-03-25 01:27:41 +0530

    languages string array added language selection spinner added language change code 30% complete

--- app/src/main/java/com/dharampravin/covidchecklist/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/dharampravin/covidchecklist/ui/dashboard/DashboardFragment.kt
@@ -1,18 +1,30 @@
 package com.dharampravin.covidchecklist.ui.dashboard
 
+import android.content.Intent
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.dharampravin.covidchecklist.R
+import com.dharampravin.covidchecklist.dashboard.view.DashboardActivity
+import java.util.*
+
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+
+    private lateinit var languageSpinner:Spinner
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +38,42 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        languageSpinner = root.findViewById(R.id.language_spinner)
+
+        languageSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                setLanguage(position)
+                Toast.makeText(activity,""+position,Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
         return root
+    }
+
+    private fun setLanguage(position: Int) {
+        var lang = "en"
+        lang = when(position) {
+            0 -> "en"
+            1 -> "hi"
+            2 -> "mr"
+            else -> "en"
+        }
+
+        val myLocale = Locale(lang)
+        val res: Resources = resources
+        val dm: DisplayMetrics = res.getDisplayMetrics()
+        val conf: Configuration = res.getConfiguration()
+        conf.locale = myLocale
+        res.updateConfiguration(conf, dm)
+        val refresh = Intent(activity, DashboardActivity::class.java)
+        activity?.finish()
+        activity?.startActivity(refresh)
     }
 }
